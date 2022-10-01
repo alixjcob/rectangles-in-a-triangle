910e9da3d6621a3237e8b56b65630c00c15054c9
Alex Jacob <59358115+alixjcob@users.noreply.github.com>
2022-10-01 15:57:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -113,4 +113,17 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
   context.restore();
 };
 
+const drawPolygon =({ context, radius =100, sides =3}) =>{
+  const slice = Math.PI * 2 / sides;
+
+  context.beginPath();
+  context.moveTo(0, -radius);
+
+  for (let i= 1; i< sides; i++){
+    const theta = i * slice;
+    context.lineTo(Math.cos(theta)* radius, Math.sin(theta)* radius);
+  }
+  context.closePath();
+}
+
 canvasSketch(sketch, settings);
